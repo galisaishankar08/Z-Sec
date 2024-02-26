ebc0fe4b16304f429d9db1bca75e55b74c9160e9
GALI SAI SHANKAR <80621346+galisaishankar08@users.noreply.github.com>
2024-02-26 13:37:23 +0000

    feat: Add functionality to handle virtual keyboard input in signup page and virtual keyboard component

--- src/app/z-auth/signup/page.tsx
+++ src/app/z-auth/signup/page.tsx
@@ -1,51 +1,270 @@
-"use client"
-import React from 'react'
-import { useState } from 'react';
-import { PublicClientApplication } from '@azure/msal-browser';
+"use client";
+import React from "react";
+import { useState } from "react";
+import { VirtualKeyboard } from "@/components";
 
-const msalConfig = {
-  auth: {
-      clientId: 'f4308d5d-6a9b-48de-a123-4ce3685cdd24', // Replace with your client ID
-      authority: 'https://login.microsoftonline.com/44c38608-c1f6-4b0d-88ea-17d9f01a8b28', // Replace with your authority URL
-      redirectUri: 'http://localhost:3000' // Replace with your redirect URI
-  }
-};
+type KeyboardValue = string;
 
-// Create a new instance of PublicClientApplication
-const pca = new PublicClientApplication(msalConfig);
-
-const page = () => {
-  const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [mfaCode, setMfaCode] = useState('');
-
-    // Define function to handle registration
-    const handleRegister = async () => {
-        try {
-            // Initiate login with Microsoft Authenticator
-            const authResult = await pca.loginPopup({
-                scopes: ['openid', 'profile', 'user.read']
-            });
-
-            // Log the access token to console (for demonstration)
-            console.log('Access token:', authResult.accessToken);
-            // You should send this access token to your backend for registration
-        } catch (error) {
-            // Handle login failure
-            console.error('Login failed:', error);
-        }
-    };
-  return (
-    <div>
-       <h3 className={`text-black text-center text-4xl font-normal`}>Signup</h3>
-        <form className={`flex flex-col gap-5 my-4`}>
-            <input type="text" id='emailorusername' className={`h-10 md:w-80 bg-[#CCC5B980] font-sans rounded-md p-2`} placeholder='Email/Username' onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" id='password' className={`h-10 md:w-80 bg-[#CCC5B980] font-sans rounded-md p-2`} placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
-            <input type="text" id='mfacode' className={`h-10 md:w-80 bg-[#CCC5B980] font-sans rounded-md p-2`} value={mfaCode} placeholder='Microsoft Authenticator Code' onChange={(e) => setMfaCode(e.target.value)}/>
-            <input type="submit" onClick={handleRegister} value="Submit" className={`bg-secondary text-primary w-1/2 mx-auto rounded-md h-10`} />
-        </form>
-    </div>
-  )
+// Define the type for the JSON object
+interface ShuffledJson {
+  [key: string]: KeyboardValue;
 }
 
-export default page
+const page = () => {
+  const [input, setInput] = useState({ email: "", password: "" });
+  const [showKeyboard, setShowKeyboard] = useState(false);
+  const [activeInput, setActiveInput] = useState("");
+
+  const defaultkeyboardvalues: KeyboardValue[] = [
+    "`",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "-",
+    "=",
+    "q",
+    "w",
+    "e",
+    "r",
+    "t",
+    "y",
+    "u",
+    "i",
+    "o",
+    "p",
+    "[",
+    "]",
+    "\\",
+    "a",
+    "s",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    ";",
+    "'",
+    "z",
+    "x",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+    ",",
+    ".",
+    "/",
+  ];
+
+  // Shuffle the array using the Durstenfeld shuffle algorithm
+  // Function to shuffle an array
+  function shuffleArray<T>(array: T[]): T[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  const shuffleddefaultArray: KeyboardValue[] = shuffleArray([
+    ...defaultkeyboardvalues,
+  ]);
+  const ShuffleddefaultKeypoard: ShuffledJson = {};
+
+  for (let i = 0; i < defaultkeyboardvalues.length; i++) {
+    ShuffleddefaultKeypoard[defaultkeyboardvalues[i]] = shuffleddefaultArray[i];
+  }
+
+  const shiftkeyboardvalues = [
+    "~",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "+",
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "Y",
+    "U",
+    "I",
+    "O",
+    "P",
+    "{",
+    "}",
+    "|",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    ":",
+    '"',
+    "Z",
+    "X",
+    "C",
+    "V",
+    "B",
+    "N",
+    "M",
+    "<",
+    ">",
+    "?",
+  ];
+
+  const shuffledShiftArray: KeyboardValue[] = shuffleArray([
+    ...shiftkeyboardvalues,
+  ]);
+  const shuffledShiftKeyboard: ShuffledJson = {};
+
+  for (let i = 0; i < shiftkeyboardvalues.length; i++) {
+    shuffledShiftKeyboard[shiftkeyboardvalues[i]] = shuffledShiftArray[i];
+  }
+  const randomkeyboardlayout = {
+    default: ShuffleddefaultKeypoard,
+    shift: shuffledShiftKeyboard,
+  };
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string,
+  ) => {
+    const inputValue: string = event.target.value.toString();
+    const lastChar = inputValue.slice(-1); // Get the last character of the input value
+
+    // Determine if the last character is in default or shift keyboard values
+    const isDefault = defaultkeyboardvalues.includes(lastChar);
+    const isShift = shiftkeyboardvalues.includes(lastChar);
+
+    let mappedValue = inputValue; // Default to the input value itself
+
+    // If the last character is in either default or shift keyboard values, map it
+    if (isDefault || isShift) {
+      const layout = isDefault
+        ? randomkeyboardlayout.default
+        : randomkeyboardlayout.shift;
+      mappedValue = layout[lastChar] || inputValue; // Fallback to inputValue if not found in the layout
+    }
+
+    setInput((prevInput) => ({
+      ...prevInput,
+      [field]: prevInput[field as keyof typeof prevInput] + mappedValue,
+    }));
+  };
+
+  const handleKeyPress = (key: any, field: string) => {
+    setInput((prevInput: { [key: string]: string }) => {
+      let updatedValue = prevInput[field];
+      switch (key) {
+        case "space":
+          updatedValue += " ";
+          break;
+        case "backspace":
+          updatedValue = updatedValue.slice(0, -1);
+          break;
+        case "clear":
+          updatedValue = "";
+          break;
+        case "enter":
+          updatedValue += "\n";
+          break;
+        case "tab":
+          updatedValue += "\t";
+          break;
+        case "close":
+          updatedValue += "";
+          setShowKeyboard(false);
+          break;
+        default:
+          updatedValue += key;
+      }
+      return {
+        email: prevInput.email,
+        password: prevInput.password,
+        [field]: updatedValue,
+      };
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Prevent the default form submission
+
+    // Perform any necessary validation or processing here
+    console.log("Form submitted:", input);
+
+    // Optionally, set the submission state to indicate that the form has been submitted
+    setShowKeyboard(false);
+  };
+
+  return (
+    <div>
+      <h3 className={`text-black text-center text-4xl font-normal`}>Signup</h3>
+      <form className={`flex flex-col gap-5 my-4`} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="emailorusername"
+          className={`h-10 md:w-80 bg-[#CCC5B980] font-sans rounded-md p-2`}
+          placeholder="Email/Username"
+          value={input.email} // Use the value from the input state
+          onChange={(e) => handleInputChange(e, "email")} // Update the input state
+          onClick={() => {
+            setShowKeyboard(true);
+            setActiveInput("email");
+          }}
+        />
+        <input
+          type="password"
+          id="password"
+          className={`h-10 md:w-80 bg-[#CCC5B980] font-sans rounded-md p-2`}
+          placeholder="Password"
+          value={input.password} // Use the value from the input state
+          onChange={(e) => handleInputChange(e, "password")} // Update the input state
+          onClick={() => {
+            setShowKeyboard(true);
+            setActiveInput("password");
+          }}
+        />
+        <input
+          type="submit"
+          value="Submit"
+          className={`bg-secondary text-primary w-1/2 mx-auto rounded-md h-10`}
+        />
+      </form>
+
+      {showKeyboard && (
+        <div className="">
+          <VirtualKeyboard
+            randomKeyboardLayout={randomkeyboardlayout}
+            onKeyPress={(key) => {
+              handleKeyPress(key, activeInput);
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default page;
--- src/components/virtualkeyboard/virtualkeyboard.tsx
+++ src/components/virtualkeyboard/virtualkeyboard.tsx
@@ -1,188 +1,85 @@
 "use client";
 import React, { useState } from "react";
 
-const VirtualKeyboard = ({ onKeyPress }) => {
-  const [input, setInput] = useState("");
-  const [capsLock, setCapsLock] = useState(false);
-  const [shift, setShift] = useState(false);
+type KeyboardValue = string;
+interface ShuffledJson {
+  [key: string]: KeyboardValue;
+}
 
-  function shuffleArray(array: any) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
+interface VirtualKeyboardProps {
+  onKeyPress: (key: string) => void;
+  randomKeyboardLayout: ShuffledJson;
+}
+
+const VirtualKeyboard: React.FC<VirtualKeyboardProps> = ({
+  onKeyPress,
+  randomKeyboardLayout,
+}) => {
+  const [shift, setShift] = useState(false);
+  console.log();
 
   const layout = {
     default: [
       "` 1 2 3 4 5 6 7 8 9 0 - = backspace",
       "tab q w e r t y u i o p [ ] \\",
-      "caps a s d f g h j k l ; ' enter",
-      "shift z x c v b n m , . / clear",
+      "caps a s d f g h j k l ; ' clear",
+      "shift z x c v b n m , . / close",
       "space",
     ],
     shift: [
       "~ ! @ # $ % ^ & * ( ) _ + backspace",
       "tab Q W E R T Y U I O P { } |",
-      'caps A S D F G H J K L : " enter',
-      "shift Z X C V B N M < > ? clear",
+      'caps A S D F G H J K L : " clear',
+      "shift Z X C V B N M < > ? close",
       "space",
     ],
   };
+  const randomkeyboardlayout: ShuffledJson = randomKeyboardLayout;
 
-  const defaultkeyboardvalues = [
-    "`",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "0",
-    "-",
-    "=",
-    "q",
-    "w",
-    "e",
-    "r",
-    "t",
-    "y",
-    "u",
-    "i",
-    "o",
-    "p",
-    "[",
-    "]",
-    "\\",
-    "a",
-    "s",
-    "d",
-    "f",
-    "g",
-    "h",
-    "j",
-    "k",
-    "l",
-    ";",
-    "'",
-    "z",
-    "x",
-    "c",
-    "v",
-    "b",
-    "n",
-    "m",
-    ",",
-    ".",
-    "/",
-  ];
-
-  const shiftkeyboardvalues = [
-    "~",
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")",
-    "_",
-    "+",
-    "Q",
-    "W",
-    "E",
-    "R",
-    "T",
-    "Y",
-    "U",
-    "I",
-    "O",
-    "P",
-    "{",
-    "}",
-    "|",
-    "A",
-    "S",
-    "D",
-    "F",
-    "G",
-    "H",
-    "J",
-    "K",
-    "L",
-    ":",
-    '"',
-    "Z",
-    "X",
-    "C",
-    "V",
-    "B",
-    "N",
-    "M",
-    "<",
-    ">",
-    "?",
-  ];
-
-  const randomkeyboardlayout = {
-    default: [],
-    shift: [],
-  };
-  randomkeyboardlayout.default = shuffleArray(defaultkeyboardvalues);
-  randomkeyboardlayout.shift = shuffleArray(shiftkeyboardvalues);
-  let k = 0;
-
-  const handleKeyPress = (key: any) => {
-    let processedKey = key;
-
-    switch (key) {
-      case "caps":
-        setCapsLock(!capsLock);
-        break;
-      case "shift":
-        setShift(!shift);
-        break;
-      default:
-        processedKey = capsLock ? key.toUpperCase() : key.toLowerCase();
-    }
-
-    if (processedKey !== "caps" && processedKey !== "shift") {
-      onKeyPress(processedKey);
+  const handleKeyPress = (key: string) => {
+    if (key === "shift" || key === "caps") {
+      setShift(!shift);
+    } else if (
+      ["backspace", "clear", "close", "space", "tab", "caps", "shift"].includes(
+        key,
+      )
+    ) {
+      onKeyPress(key);
+    } else {
+      const mappedKey =
+        randomKeyboardLayout[shift ? "shift" : "default"][key] || key;
+      onKeyPress(mappedKey);
     }
   };
-
   return (
-    <div>
-      <div className="grid grid-flow-row gap-2 h-fit w-fit bg-gray-200 rounded-md p-2 border-4 border-red-500">
-        {layout[capsLock || shift ? "shift" : "default"].map((row, i) => (
+    <div className="mb-4 fixed bottom-0 left-1/2 transform -translate-x-1/2">
+      <div className="grid grid-flow-row gap-2 h-fit w-fit bg-gray-200 rounded-md p-2 border-4 border-secondary font-sans">
+        {layout[shift ? "shift" : "default"].map((row, i) => (
           <div key={i} className="grid grid-flow-col gap-2">
             {row.split(" ").map((key, j) => (
               <button
                 key={j}
                 onClick={() => handleKeyPress(key)}
-                className={`bg-white border-2 border-gray-300 p-2 rounded-md min-w-10 col-auto ${
-                  shift && key === "shift" ? "bg-red-600" : ""
-                } ${capsLock && key === "caps" ? "bg-red-600" : ""}`}
+                className={`bg-white border-2 border-gray-300 p-2 rounded-md min-w-10 col-auto shadow-[0_0_5px_#F7FEE7,0_0_10px_#F7FEE7,0_0_15px_#F7FEE7,0_0_25px_#F7FEE7] ${
+                  shift && key === "shift" ? "bg-red-500" : ""
+                } ${shift && key === "caps" ? "bg-red-500" : ""} ${key === "close" ? "bg-red-500" : ""}`}
               >
-                {key === "backspace" ||
-                key === "clear" ||
-                key === "enter" ||
-                key === "space" ||
-                key === "tab" ||
-                key === "caps" ||
-                key === "shift" ? (
+                {[
+                  "backspace",
+                  "clear",
+                  "close",
+                  "space",
+                  "tab",
+                  "caps",
+                  "shift",
+                ].includes(key) ? (
                   key
                 ) : (
                   <div className="relative grid grid-flow-col space-x-2">
                     <p className="absolute top-0 right-0-0 text-xs">{key}</p>
-                    <p className="">{randomkeyboardlayout.default[i + j]}</p>
+                    <p className="text-secondary font-bold text-md">
+                      {randomkeyboardlayout[shift ? "shift" : "default"][key]}
+                    </p>
                   </div>
                 )}
               </button>
